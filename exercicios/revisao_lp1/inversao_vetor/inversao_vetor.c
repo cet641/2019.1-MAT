f8b9e811eb9f1ceb241080a9c6a17384b963e7ac
Mathias Santos de Brito <mathias.brito@me.com>
2019-04-05 11:12:29 -0300

    exercicion inversao_vetor

---
+++ exercicios/revisao_lp1/inversao_vetor/inversao_vetor.c
@@ -0,0 +1,15 @@
+/* Dado um vetor como etnrada, o seu programa deverá inverter o vetor, ou seja
+ * o primeiro valor deverá passar a ser o último, o segundo o penúltimo e
+ * assim por diante. Leia os valores do vetor a partir do teclado.
+ */
+
+#include <stdio.h>
+
+#define TAM_VETOR 5
+
+int main() {
+
+    /* escreva a solução aqui */
+
+    return 0;
+}
