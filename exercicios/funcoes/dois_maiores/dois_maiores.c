a7600d6dfc7123189daff966f63f64dc28bfd311
Mathias Santos de Brito <mathias.brito@me.com>
2019-04-08 14:31:22 -0300

    exercício dois maiores e reorganização de pastas'

---
+++ exercicios/funcoes/dois_maiores/dois_maiores.c
@@ -0,0 +1,16 @@
+/* Crie um programa que retorne os dois maiores valores encontrados em um
+ * vetor. Você deverá implementar o programa em uma função própria, não 
+ * na função main. A função receberá um vetor e duas variáveis que irão
+ * receber os valores encontrados. A função é do tipo void e não retorna
+ * valores com return.
+ */
+
+
+int vetor = {34, 32, 68, 93, 45, 11, 72, 2, 39 };
+
+/* Implemente sua Função aqui */
+
+int main() {
+    /* Chame sua função aqui */
+    return 0;
+}
--- exercicios/revisao_lp1/maior_de_dois_numeros/maior_de_dois_numeros.c
+++ exercicios/revisao_lp1/maior_de_dois_numeros/maior_de_dois_numeros.c
@@ -1,6 +1,6 @@
 /* Escreva um programa que receba dois números inteiros e imprima na tela
  * o maior desses dois números. Ex. dado os numeros 55 e 83, o númer 83
- * deve ser impresso na tela.
+ * deve ser impresso na tela. Caso os números sejam iguais imprima "Iguais".
  */
 
 #include <stdio.h>
