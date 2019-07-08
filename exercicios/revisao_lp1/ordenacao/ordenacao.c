3e8cd7ea516207d24e4a290f715dea34a490892c
Mathias Santos de Brito <mathias.brito@me.com>
2019-07-08 15:10:56 -0300

    exercício ordenação

---
+++ exercicios/revisao_lp1/ordenacao/ordenacao.c
@@ -0,0 +1,13 @@
+/* Um vetor com diferentes números nem sempre estará com os números em ordem
+ * seja decrescente ou crescente. Uma tarefa comum na computação é ordenar 
+ * estes numeros. Dado um vetor com diferentes números e fora de ordem,
+ * ordene os números em ordem crescente. */
+
+int vetor[] = {6, 3, 5, 1, 8};
+
+#include <stdio.h>
+
+int main() {
+    /* Solução Aqui */
+    return 0;
+}
